313183f2f1271792afb98922075ca4ecd13148d3
Raymond Machira <raymond_m@apple.com>
2019-10-29 21:13:06 -0400

    add functional stacker

---
+++ funstacker.rkt
@@ -0,0 +1,34 @@
+;#lang reader stacker-demo/stacker
+;4
+;8
+;+
+;3
+;*
+
+#lang br/quicklang
+
+(define (read-syntax path port)
+  (define src-lines (port->lines port))
+  (define src-datums (format-datums '~a src-lines))
+  (define module-datum `(module stacker-mod "funstacker.rkt" (handle-args ,@src-datums)))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
+
+;(define (handle [a #f]) (print a))
+(define-macro (fun-stacker-module-begin HANDLE-ARGS-EXPR)
+  #'(#%module-begin (display (first HANDLE-ARGS-EXPR))))
+(provide (rename-out [fun-stacker-module-begin #%module-begin]))
+
+
+(define (handle-args . args)
+  (for/fold
+   ([stack empty])
+   ([arg (in-list args)] #:unless (void? arg))
+  (cond
+    [(number? arg) (cons arg stack)]
+    [(or (equal? + arg) (equal? * arg))
+     (define op-result (arg (first stack) (second stack)))
+     (cons op-result (drop stack 2))])))
+
+(provide handle-args)
+(provide + *)
