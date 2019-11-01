776023e806f5a14289964f3c317b01bb8d3eda42
Raymond Machira <raymond_m@apple.com>
2019-10-31 21:29:19 -0400

    create bf package

--- bf/reader.rkt
+++ bf/reader.rkt
@@ -3,8 +3,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module bf-mod "fnexpander.rkt"
-                          ,parse-tree))
+  (define module-datum `(module bf-mod bf/fnexpander ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
 
